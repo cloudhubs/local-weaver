6e3a6f8525fa67571e32f5fb1c1cc6f1e5e30a55
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-09-05 17:57:54 -0500

    Adds JSON parsing - required reformatting the map #SEER-4

--- evaluator/src/main/java/edu/baylor/ecs/cfgg/evaluator/service/EvaluatorService.java
+++ evaluator/src/main/java/edu/baylor/ecs/cfgg/evaluator/service/EvaluatorService.java
@@ -1,6 +1,8 @@
 package edu.baylor.ecs.cfgg.evaluator.service;
 
 import edu.baylor.ecs.cfgg.evaluator.repository.LoaderRepository;
+import edu.baylor.ecs.cfgg.evaluator.service.models.MethodModel;
+import javafx.util.Pair;
 import javassist.*;
 import javassist.bytecode.MethodInfo;
 import javassist.expr.ExprEditor;
@@ -8,10 +10,7 @@ import javassist.expr.MethodCall;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class EvaluatorService {
@@ -19,17 +18,21 @@ public class EvaluatorService {
     @Autowired
     private LoaderRepository loaderRepository;
 
-    private Map<String, ArrayList<String>> map;
+    private Map<String, ArrayList<MethodModel>> map;
 
     public String deriveApplicationStructure(){
+
+        // Setup some initial objects
         String classes = loaderRepository.getClasses();
         map = new HashMap<>();
-        String applicationStructureInJson = "";
-
+        String applicationStructureInJson;
         String[] classArr = classes.split(":");
-
         ClassPool cp = ClassPool.getDefault();
+
+        // Loop through every class in the array
         for(String className : classArr){
+
+            // Try to get the class as a CtClass
             CtClass clazz = null;
             try {
                 clazz = cp.get(className);
@@ -37,35 +40,51 @@ public class EvaluatorService {
                 System.out.println(e.toString());
                 break;
             }
+
+            // Store the key for the calling class
+            String key = clazz.toString();
+
+            // If this class isn't in the map, store it with a new list
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+
             // Retrieve all the methods of a class
             CtMethod[] methods = clazz.getDeclaredMethods();
 
             // Loop through every method
             for(CtMethod method : methods){
 
-                // Store the className and methodName for building the key within the inner class
-                String innerClassName = clazz.getName();
-                String innerMethodName = method.getName();
+                // Retrieve the list of all method mappings from the class
+                ArrayList<MethodModel> list = map.get(key);
 
-                // Build the key for this method
-                String key = getKey(innerClassName, innerMethodName);
-
-                // If this class:method isn't in the map, store it with a new list
-                if(!map.containsKey(key)){
-                    map.put(key, new ArrayList<>());
+                // Loop through and check each of the MethodModels to see if it corresponds to the current method
+                boolean found = false;
+                for(MethodModel methodModel : list){
+                    if(methodModel.getMethod().equals(method)){
+                        // If the method model is for this method then no need to add it
+                        found = true;
+                    }
                 }
 
+                // If there isnt a MethodModel for this method, add it
+                if(!found){
+                    MethodModel methodModel = new MethodModel(method);
+                    list.add(methodModel);
+                }
+
+                // Instrument the method to pull out the method calls
                 try {
-                    // Instrument the method to pull out the method calls
                     method.instrument(
                         new ExprEditor() {
                             public void edit(MethodCall m) {
 
-                                // Store the inner method call within the map
-                                ArrayList<String> list = map.get(key);
-                                list.add(getKey(m.getClassName(), m.getMethodName()));
-
-                                System.out.println(getKey(m.getClassName(), m.getMethodName()));
+                                for(MethodModel methodModel : list){
+                                    if(methodModel.getMethod().equals(method)){
+                                        // If the method model is for this method then add the sub method
+                                        methodModel.addSubMethod(m.getClassName(), m.getMethodName());
+                                    }
+                                }
                             }
                         }
                     );
@@ -75,11 +94,32 @@ public class EvaluatorService {
             }
         }
 
+        // Build the JSON and return it
+        applicationStructureInJson = buildJson(map);
         return applicationStructureInJson;
     }
 
-    private String getKey(String className, String methodName){
-        return className.concat(":").concat(methodName);
-    }
+    private String buildJson(Map<String, ArrayList<MethodModel>> map) {
+        String JSON = "";
 
+        Iterator<String> it = map.keySet().iterator();
+        while (it.hasNext()) {
+            String className = it.next();
+            JSON = JSON.concat("\"").concat(className).concat("\" { \n");
+            List<MethodModel> methodModelList = map.get(className);
+            for(MethodModel methodModel : methodModelList){
+                JSON = JSON.concat("\t\"").concat(methodModel.getMethod().getName()).concat("\" : { \n");
+
+                for(Pair<String, String> subMethod : methodModel.getSubMethods()){
+                    JSON = JSON.concat("\t\t\"").concat(subMethod.getKey()).concat("\":\"").concat(subMethod.getValue()).concat("\"\n");
+                }
+
+                JSON = JSON.concat("\t} \n");
+            }
+
+            JSON = JSON.concat("} \n");
+        }
+
+        return JSON;
+    }
 }
