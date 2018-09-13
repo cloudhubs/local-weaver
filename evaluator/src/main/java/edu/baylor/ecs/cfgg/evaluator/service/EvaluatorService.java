6289b038b1377ef26bbb65e77bdfe4a007e7212b
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-09-12 19:24:18 -0500

    Loads classes from jar to build JSON

--- evaluator/src/main/java/edu/baylor/ecs/cfgg/evaluator/service/EvaluatorService.java
+++ evaluator/src/main/java/edu/baylor/ecs/cfgg/evaluator/service/EvaluatorService.java
@@ -3,10 +3,8 @@ package edu.baylor.ecs.cfgg.evaluator.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.baylor.ecs.cfgg.evaluator.repository.EvaluatorRepository;
 import javassist.*;
-import javassist.bytecode.CodeAttribute;
-import javassist.bytecode.CodeIterator;
-import javassist.bytecode.MethodInfo;
-import javassist.bytecode.Mnemonic;
+import javassist.bytecode.*;
+import javassist.bytecode.analysis.FramePrinter;
 import javassist.expr.ExprEditor;
 import javassist.expr.MethodCall;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,31 +19,54 @@ public class EvaluatorService {
     private EvaluatorRepository evaluatorRepository;
 
     private Map<List<String>, List<List<String>>> formattedMap;
+    ClassPathScanner classPathScanner = new ClassPathScanner();
+    Set<ClassFile> classFileSet;
 
     public String deriveApplicationStructure(){
 
-        // Setup some initial objects
-        formattedMap = new HashMap<>();
-        String applicationStructureInJson = "";
         ClassPool cp = ClassPool.getDefault();
 
-        // Loop through every class in the array
-        for(Class className : evaluatorRepository.getClasses()){
+        classPathScanner.scanUri("file://///Users/walkerand/Documents/Research/sm-core-2.2.0-SNAPSHOT.jar");
+        classFileSet = classPathScanner.getClasses();
 
+        List<CtClass> classes = new ArrayList<>();
+        for(ClassFile classFile : classFileSet){
             // Try to get the class as a CtClass
             CtClass clazz = null;
             try {
-                clazz = cp.get(className.getName());
+                clazz = cp.makeClass(classFile);
             } catch (Exception e){
                 System.out.println(e.toString());
                 break;
             }
+            classes.add(clazz);
+        }
+
+        // Setup some initial objects
+        formattedMap = new HashMap<>();
+        String applicationStructureInJson = "";
+
+        FramePrinter fp = new FramePrinter(System.out);
+
+        // Loop through every class in the array
+        for(CtClass clazz : classes){
+        //for(Class className : evaluatorRepository.getClasses()){
+
+//            CtClass clazz = null;
+//            try {
+//                clazz = cp.get(className.getName());
+//            } catch (Exception e){
+//                System.out.println(e.toString());
+//                break;
+//            }
 
             // Retrieve all the methods of a class
             CtMethod[] methods = clazz.getDeclaredMethods();
 
             // Loop through every method
             for(CtMethod method : methods){
+
+                //fp.print(method);
 
                 // Build the key for the formattedMap
                 ArrayList<String> formattedKey = new ArrayList<>();
