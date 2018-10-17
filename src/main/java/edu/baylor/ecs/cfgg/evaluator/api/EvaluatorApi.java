847632a694e2a4826ef78dc57822966a270547fc
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-10-17 16:14:25 -0500

    Trims down the project to a proof of concept

--- src/main/java/edu/baylor/ecs/cfgg/evaluator/api/EvaluatorApi.java
+++ src/main/java/edu/baylor/ecs/cfgg/evaluator/api/EvaluatorApi.java
@@ -1,8 +1,6 @@
 package edu.baylor.ecs.cfgg.evaluator.api;
 
 import edu.baylor.ecs.cfgg.evaluator.service.ApplicationEvaluatorService;
-import edu.baylor.ecs.cfgg.evaluator.service.BytecodeEvaluatorService;
-import edu.baylor.ecs.cfgg.evaluator.service.DependencyEvaluatorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/edu/baylor/ecs/cfgg/evaluator/repository/EvaluatorRepository.java
+++ src/main/java/edu/baylor/ecs/cfgg/evaluator/repository/EvaluatorRepository.java
@@ -1,7 +1,5 @@
 package edu.baylor.ecs.cfgg.evaluator.repository;
 
-import edu.baylor.ecs.cfgg.evaluator.mock.MockClassA;
-import edu.baylor.ecs.cfgg.evaluator.mock.MockClassB;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -12,8 +10,7 @@ public class EvaluatorRepository {
 
     public List<Class> getClasses(){
         List<Class> classes = new ArrayList<>();
-        classes.add(MockClassA.class);
-        classes.add(MockClassB.class);
+
         return classes;
     }
 }
