6e9648a6c860029e9d9539d42fe27099276bf961
John Diehl <jrd1250@gmail.com>
2018-10-29 19:14:44 -0500

    Modified SecurityEvaluatorService to use new security evaluation design

--- library/src/main/java/edu/baylor/ecs/seer/lweaver/service/SecurityEvaluatorService.java
+++ library/src/main/java/edu/baylor/ecs/seer/lweaver/service/SecurityEvaluatorService.java
@@ -5,6 +5,7 @@ import javassist.CannotCompileException;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.MethodInfo;
 import javassist.bytecode.annotation.Annotation;
 import javassist.bytecode.annotation.ArrayMemberValue;
 import javassist.bytecode.annotation.MemberValue;
@@ -17,67 +18,21 @@ import java.util.*;
 @Service
 public class SecurityEvaluatorService extends EvaluatorService {
 
-    private Map<String, List<CtClass>> roles = new HashMap<>();
+    private Map<String, List<String>> roles = new HashMap<>();
+    private Map<String, List<String>> nodes = new HashMap<>();
 
     protected final String process(List<CtClass> classes){
         // Setup the return string
         String applicationStructureInJson = "";
 
-        // Loop through all the roles
-        for(String key : roles.keySet()) {
-
-            // Setup some initial objects
-            Map<List<String>, List<List<String>>> formattedMap = new HashMap<>();
-
-            // Loop through every class the role has access to
-            for (CtClass clazz : roles.get(key)) {
-
-                // Retrieve all the methods of a class
-                CtMethod[] methods = clazz.getDeclaredMethods();
-
-                // Loop through every method
-                for (CtMethod method : methods) {
-
-                    // Build the key for the formattedMap
-                    ArrayList<String> formattedKey = new ArrayList<>();
-                    formattedKey.add(clazz.getName());
-                    formattedKey.add(method.getName());
-
-                    // Add the formattedKey to the formattedMap
-                    formattedMap.put(formattedKey, new ArrayList<>());
-
-
-                    // Instrument the method to pull out the method calls
-                    try {
-                        method.instrument(
-                            new ExprEditor() {
-                                public void edit(MethodCall m) {
-
-                                    // Retrieve the list of subMethods
-                                    List<List<String>> subMethodList = formattedMap.get(formattedKey);
-
-                                    // Build the key for the subMethod
-                                    ArrayList<String> subMethodKey = new ArrayList<>();
-                                    subMethodKey.add(m.getClassName());
-                                    subMethodKey.add(m.getMethodName());
-
-                                    subMethodList.add(subMethodKey);
-                                }
-                            }
-                        );
-                    } catch (CannotCompileException e) {
-                        System.out.println(e.toString());
-                    }
-                }
-            }
-
-            try {
-                String processedJSON = new ObjectMapper().writeValueAsString(formattedMap);
-                applicationStructureInJson = applicationStructureInJson.concat(key + "\n");
-                applicationStructureInJson = applicationStructureInJson.concat(processedJSON + "\n");
-            } catch (Exception e) {
-                System.out.println(e.toString());
-            }
+        try {
+            applicationStructureInJson = new ObjectMapper().writeValueAsString(roles);
+            applicationStructureInJson = applicationStructureInJson.concat("\n@");
+            String s = new ObjectMapper().writeValueAsString(nodes);
+            applicationStructureInJson = applicationStructureInJson.concat(s);
+            applicationStructureInJson = applicationStructureInJson.concat("\n");
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
 
         return applicationStructureInJson;
@@ -86,44 +41,84 @@ public class SecurityEvaluatorService extends EvaluatorService {
     // This filter will be slightly different than the others in that we want to break up the sorting into multiple
     // lists of CtClass objects by role. This filter function will be used to cache the classes in a private map
     // which will then be used in process
-    protected final boolean filter(CtClass clazz){
+    protected final boolean filter(CtClass clazz) {
         AnnotationsAttribute annotationsAttribute = (AnnotationsAttribute) clazz.getClassFile().getAttribute(AnnotationsAttribute.visibleTag);
         if(annotationsAttribute != null) {
-            Annotation[] annotations = annotationsAttribute.getAnnotations();
-            for (Annotation annotation : annotations) {
-                if (annotation.getTypeName().equals("javax.annotation.security.RolesAllowed")) {
+            Annotation[] classAnnotations = annotationsAttribute.getAnnotations();
+            Annotation[] methodAnnotations = null;
 
-                    // Get the names and loop
+            List<String> defaultPerms = new ArrayList<>();
+            for (Annotation annotation : classAnnotations) {
+                if (annotation.getTypeName().equals("javax.annotation.security.RolesAllowed")) {
                     Set<String> names = annotation.getMemberNames();
                     for (String name : names) {
-                        // Get the value for the name
                         MemberValue value = annotation.getMemberValue(name);
-
-                        if(value instanceof ArrayMemberValue){
+                        if (value instanceof ArrayMemberValue) {
                             ArrayMemberValue amv = (ArrayMemberValue)value;
                             MemberValue[] memberValues = amv.getValue();
-                            for(MemberValue mv : memberValues){
-                                String key = mv.toString().replace("\"", "");
-                                if(roles.get(key) == null) {
-                                    roles.put(key, new ArrayList<>());
-                                }
-                                roles.get(key).add(clazz);
+                            for (MemberValue mv : memberValues) {
+                                String val = mv.toString().replace("\"", "");
+                                defaultPerms.add(val);
                             }
                         }
                     }
-                    return true;
-                } else if (annotation.getTypeName().equals("javax.annotation.security.PermitAll")){
-                    Iterator it = roles.keySet().iterator();
-                    while (it.hasNext()) {
-                        String key = (String)it.next();
-                        List<CtClass> list = roles.get(key);
-                        list.add(clazz);
-                    }
-                    return true;
+                }
+            }
 
+            for ( CtMethod method : clazz.getDeclaredMethods() ) {
+                MethodInfo methodInfo = method.getMethodInfo();
+                AnnotationsAttribute attr = (AnnotationsAttribute) methodInfo.getAttribute(AnnotationsAttribute.visibleTag);
+                methodAnnotations = attr.getAnnotations();
+                nodes.put(method.getLongName(), new ArrayList<>());
+                roles.put(method.getLongName(), new ArrayList<>());
+                for (Annotation annotation : methodAnnotations) {
+                    if (annotation.getTypeName().equals("javax.annotation.security.RolesAllowed")) {
+                        Set<String> names = annotation.getMemberNames();
+                        for (String name : names) {
+                            MemberValue value = annotation.getMemberValue(name);
+                            if (value instanceof ArrayMemberValue) {
+                                ArrayMemberValue amv = (ArrayMemberValue)value;
+                                MemberValue[] memberValues = amv.getValue();
+                                for (MemberValue mv : memberValues) {
+                                    String val = mv.toString().replace("\"", "");
+                                    roles.get(method.getLongName()).add(val);
+                                }
+                            }
+                        }
+                    } else if (annotation.getTypeName().equals("javax.annotation.security.PermitAll")) {
+                        if (defaultPerms.size() > 0) {
+                            roles.get(method.getLongName()).addAll(defaultPerms);
+                        } else {
+                            roles.get(method.getLongName()).add("SEER_DEFAULT_ALL_ROLES_PERMITTED");
+                        }
+                    }
+
+                    if (annotation.getTypeName().equals("javax.annotation.security.RolesAllowed") ||
+                        annotation.getTypeName().equals("javax.annotation.security.PermitAll")) {
+                        try {
+                            method.instrument(
+                                    new ExprEditor() {
+                                        public void edit(MethodCall m) {
+                                            List<String> subMethodList = nodes.get(method.getLongName());
+
+                                            CtMethod ctMethod;
+                                            try {
+                                                ctMethod = m.getMethod();
+                                            } catch (Exception ex) {
+                                                return;
+                                            }
+                                            subMethodList.add(ctMethod.getLongName());
+                                        }
+                                    }
+                            );
+                        } catch (CannotCompileException cex) {
+                            System.out.println(cex.toString());
+                            return false;
+                        }
+                    }
                 }
             }
         }
-        return false;
+        return true;
     }
 }
