8fd54d9254a57b1730cbadac01246c717c76ba7f
Jan Svacina <jan_svacina2@baylor.edu>
2019-04-11 22:54:32 -0500

    api context service

--- library/src/main/java/edu/baylor/ecs/seer/lweaver/service/SeerMsApiContextService.java
+++ library/src/main/java/edu/baylor/ecs/seer/lweaver/service/SeerMsApiContextService.java
@@ -1,29 +1,92 @@
 package edu.baylor.ecs.seer.lweaver.service;
 
+import edu.baylor.ecs.seer.common.api.SeerApiMethod;
+import edu.baylor.ecs.seer.common.api.SeerApiType;
+import edu.baylor.ecs.seer.common.context.SeerApiContext;
+import edu.baylor.ecs.seer.common.entity.EntityModel;
 import javassist.CtClass;
 import javassist.CtMethod;
 import javassist.NotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SeerMsApiContextService {
 
-
-    public void getMethods(List<CtClass> ctClassesApiIn){
+    public SeerApiContext createSeerApiContext(List<CtClass> ctClassesApiIn){
+        List<SeerApiMethod> apiMethods = new ArrayList<>();
         for (CtClass ctClass: ctClassesApiIn){
             CtMethod[] ctMethods = ctClass.getMethods();
             for (CtMethod ctMethod: ctMethods
                  ) {
-                //Parameter Types
-                try {
-                    ctMethod.getParameterTypes();
-                } catch (NotFoundException e) {
-                    e.printStackTrace();
-                }
-                //
-                ctMethod.getReturnType();
-
+                apiMethods.add(createSeerApiMethod(ctClass, ctMethod));
             }
         }
+        SeerApiContext seerApiContext = new SeerApiContext();
+        return seerApiContext;
     }
+
+    public SeerApiMethod createSeerApiMethod(CtClass ctClass, CtMethod ctMethod){
+        SeerApiMethod seerApiMethod = new SeerApiMethod();
+        seerApiMethod.setClassName(ctClass.getName());
+        seerApiMethod.setMethodName(ctMethod.getLongName());
+        seerApiMethod.setSeerApiType(getSeerApiType(ctMethod));
+        if (seerApiMethod.getSeerApiType().equals(SeerApiType.OUT)){
+            seerApiMethod.setEntityModel(getReturnType(ctMethod));
+        } else {
+            seerApiMethod.setEntityModel(getParameterType(ctMethod));
+        }
+        return seerApiMethod;
+    }
+
+    private EntityModel getParameterType(CtMethod ctMethod) {
+        EntityModel entityModel = new EntityModel();
+        try {
+            CtClass[] parameterTypes = ctMethod.getParameterTypes();
+            for (CtClass p: parameterTypes) {
+                System.out.println(p.getName());
+                entityModel.setClassNameShort(p.getSimpleName());
+                entityModel.setClassName(p.getName());
+            }
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return entityModel;
+    }
+
+    private EntityModel getReturnType(CtMethod ctMethod) {
+        EntityModel entityModel = new EntityModel();
+        try {
+            CtClass ctClass = ctMethod.getReturnType();
+            entityModel.setClassNameShort(ctClass.getSimpleName());
+            entityModel.setClassName(ctClass.getName());
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
+        return entityModel;
+    }
+
+
+    private SeerApiType getSeerApiType(CtMethod ctMethod) {
+        Object[] annotations = new Object[]{};
+        SeerApiType seerApiType = SeerApiType.IN;
+        try {
+            annotations = ctMethod.getAnnotations();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        for (Object annotation: annotations
+             ) {
+            if (annotation.getClass().getName().equals("javax.ws.rs.GET")){
+                seerApiType = SeerApiType.OUT;
+            } else if (annotation.getClass().getName().equals("javax.ws.rs.POST")){
+                seerApiType = SeerApiType.IN;
+            }
+        }
+        return seerApiType;
+    }
+
+
 }
